d92b8ff56038e2e5e985117ff39420d90ede1a53
DMLen <dmlennox03@gmail.com>
2024-05-20 11:59:29 +1000

    new methods

--- player.py
+++ player.py
@@ -5,17 +5,26 @@ class Player():
         self._score = 0
     
     def __str__(self):
-        return f"Player {self.name} currently has {self._turnpoints} turn points, and a total score of {self._score}!"
+        return f"Player {self.name}"
     
     def addTurnPoints(self, points):
-        self._turntotal += points
+        self._turnpoints += points
     
     def clearTurnPoints(self):
-        self._turntotal = 0
+        self._turnpoints = 0
 
     def bankTurnPoints(self):
-        self._score += self._turntotal
-        self.clearTurnTotal()
+        self._score += self._turnpoints
+        self.clearTurnPoints()
 
     def getScore(self):
         return self._score
+    
+    def getTurnPoints(self):
+        return self._turnpoints
+
+    def printInfo(self):
+        print(f"Player {self.name} currently has a total score of {self._score}!")
+    
+    def printTurnInfo(self):
+        print(f"You currently have {self._turnpoints} turn points, and a total score of {self._score}!")
